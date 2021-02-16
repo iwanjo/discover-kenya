83814a66acc733792d395f81e18fcaa07ea35f0f
ahmedmeshref <a.meshref@alustudent.com>
2021-02-16 14:37:43 +0200

    fix margin of the GridView to match the rest of the app

--- discover_kenya/lib/components/grid.item.dart
+++ discover_kenya/lib/components/grid.item.dart
@@ -1,35 +1,21 @@
 import 'package:flutter/material.dart';
 
 class GridItem extends StatelessWidget {
-  final String title;
-  final IconData iconName;
+  final String imageName;
 
-  GridItem({Key key, this.title, this.iconName}) : super(key: key);
+  GridItem({Key key, this.imageName}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.all(0),
+      padding: EdgeInsets.all(0.0),
       child: Card(
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
         color: Theme.of(context).primaryColor,
-        shadowColor: Colors.orange,
 
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Icon(
-              iconName,
-              color: Colors.white,
-              size: 70,
-            ),
-            SizedBox(
-              height: 4.0,
-            ),
-            Text(
-              title,
-              style: TextStyle(color: Colors.white, fontSize: 15),
-            )
           ],
         ),
       ),
--- discover_kenya/lib/components/home_grid.dart
+++ discover_kenya/lib/components/home_grid.dart
@@ -5,22 +5,21 @@ import 'package:discover_kenya/components/grid.item.dart';
 class HomeGrid extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView.count(
-      shrinkWrap: true,
-      physics: NeverScrollableScrollPhysics(),
-      crossAxisCount: 2,
-      crossAxisSpacing: 10.0,
-      mainAxisSpacing: 8.0,
-      children: [
-        GridItem(
-          title: 'Account',
-          iconName: Icons.account_box,
-        ),
-        GridItem(
-          title: 'Lunch',
-          iconName: Icons.lunch_dining,
-        ),
-      ],
+    return Container(
+      margin: EdgeInsets.all(25.0),
+      child: GridView.count(
+        shrinkWrap: true,
+        physics: NeverScrollableScrollPhysics(),
+        crossAxisCount: 2,
+        crossAxisSpacing: 5.0,
+        mainAxisSpacing: 8.0,
+        children: [
+          GridItem(
+          ),
+          GridItem(
+          ),
+        ],
+      ),
     );
   }
 
